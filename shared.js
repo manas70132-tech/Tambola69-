513d995ef797d6397be918b55ec0e30a3715b512
manas70132-tech <manas70132@gmail.com>
2025-09-03 02:33:12 +0530

    Update shared.js
    Signed-off-by: manas70132-tech <manas70132@gmail.com>

--- shared.js
+++ shared.js
@@ -1,12 +1,12 @@
 // Firebase config (apna config paste karo)
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyAphHTM4faH7_FVXdnootp4NipZd3vtEck",
+  authDomain: "tambola69-9dc8f.firebaseapp.com",
+  databaseURL: "https://tambola69-9dc8f.firebaseio.com",
+  projectId: "tambola69-9dc8f",
+  storageBucket: "tambola69-9dc8f.appspot.com",
+  messagingSenderId: "413290158586",
+  appId: "1:413290158586:web:70fff2d94b29206487ec43"
 };
 
 // Initialize Firebase (sirf ek baar)
