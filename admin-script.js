481dff63343ec891f1b3933a1ed0b1e9f7b1374b
manas70132-tech <manas70132@gmail.com>
2025-09-05 20:46:28 +0530

    Update admin-script.js
    Signed-off-by: manas70132-tech <manas70132@gmail.com>

--- admin-script.js
+++ admin-script.js
@@ -1,6 +1,10 @@
 /* ================= Firebase Init (REPLACE WITH YOUR CONFIG) ================= */
-const firebaseConfig = {
-  // YOUR CONFIG GOES HERE
+const firebaseConfig = {apiKey: "AIzaSyAphHTM4faH7_FVXdnootp4NipZd3vtEck",
+  authDomain: "tambola69-9dc8f.firebaseapp.com",
+  projectId: "tambola69-9dc8f",
+  storageBucket: "tambola69-9dc8f.firebasestorage.app",
+  messagingSenderId: "413290158586",
+  appId: "1:413290158586:web:70fff2d94b29206487ec43",
 };
 
 // Initialize Firebase
