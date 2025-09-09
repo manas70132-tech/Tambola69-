f7fdc7e5f25a6ac3f2a57638cb2a1bf4ebc66a13
manas70132-tech <manas70132@gmail.com>
2025-09-10 04:19:24 +0530

    Update firebase-config.js
    Signed-off-by: manas70132-tech <manas70132@gmail.com>

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,15 @@
 // Replace below config with your Firebase project keys
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyAphHTM4faH7_FVXdnootp4NipZd3vtEck",
+  authDomain: "tambola69-9dc8f.firebaseapp.com",
+  databaseURL: "https://tambola69-9dc8f-default-rtdb.firebaseio.com",
+  projectId: "tambola69-9dc8f",
+  storageBucket: "tambola69-9dc8f.firebasestorage.app",
+  messagingSenderId: "413290158586",
+  appId: "1:413290158586:web:70fff2d94b29206487ec43",
+  measurementId: "G-6WJNDX5DGB"
 };
+  
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
