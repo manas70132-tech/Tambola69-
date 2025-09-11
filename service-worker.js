73f5425de6924ea34c24fa0eff26826f34b06d75
manas70132-tech <manas70132@gmail.com>
2025-09-12 00:43:26 +0530

    Update service-worker.js
    Signed-off-by: manas70132-tech <manas70132@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,30 @@
-self.addEventListener("install",event
-=> {
-event.waituntill(
-caches.open("Tambola-cache").then(cache
-=>{
-return cache.addall([
-"/
-"
-"/index.html",
-"/admin.html",
-"/style.css",
-"/script.js",
-"/icon-192.png",
-"/icon-512.png",
-]);
-})
-);
+hereconst CACHE_NAME = 'tambola-69-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js',
+  'https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js',
+  'https://www.gstatic.com/firebasejs/8.10.1/firebase-database.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEvenListener("fetch",event=>
-{
-event.respondWith(
-caches.match(event.request).then(response =>{
-return response ||
-fetch(event.request);
-})
-);
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
 });
