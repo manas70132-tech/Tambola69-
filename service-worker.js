4a0e63d43f07f1ac7b7220a51bdc62b0289251c1
manas70132-tech <manas70132@gmail.com>
2025-10-14 21:58:32 +0530

    Update service-worker.js
    Signed-off-by: manas70132-tech <manas70132@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,14 @@
-           const CACHE_NAME = 'tambola69-v1';
-const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/offline.html',
-  '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png',
-  // add other static assets you want cached (css, js, images)
-];
-
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+    caches.open('tambola-v1').then(cache => {
+      return cache.addAll(['/', '/index.html', '/icon-192x192.png', '/icon-512x512.png']);
+    })
   );
-  self.skipWaiting();
 });
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }))
-    )
-  );
-  self.clients.claim();
-});
-
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(cachedResp => {
-      if (cachedResp) return cachedResp;
-      return fetch(event.request).catch(() => caches.match('/offline.html'));
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
